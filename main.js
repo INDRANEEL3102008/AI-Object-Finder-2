83f5dcf8befacaa3529b39d8420d49922240e6df
INDRANEEL3102008 <77006195+INDRANEEL3102008@users.noreply.github.com>
2021-07-10 17:37:16 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+function setup() {
+    canvas = createCanvas(500, 380);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(500, 380);
+    video.hide();
+}
+function start() {
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Object";
+    objectName = document.getElementById("ibox").value;
+
+}
+
+function modelLoaded() {
+    console.log("Model Loaded!");
+    status = true;
+}
+function draw() {
+    image(video, 0, 0, 500, 380);
+    if(status !="") {
+        objectDetector.detect(video, gotResult);
+        for (i = 0; i < objects.length; i++) {
+            document.getElementById("status").innerHTML = "Status : Object detected";
+            fill("#FF0000");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + "" + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+
+        }
+    }
+}
